c156bbe0d56e0e8a300a8d64e0569beace2b4132
mahafathy187@gmail.com <mahafathyh@gmail.com>
2024-01-07 13:30:07 +0200

    mail me directly

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -16,6 +16,6 @@ linkedInLink= 'https://www.linkedin.com/in/maha-fathy-40801b1b5/'
     this.titleService.setTitle('Maha Fathy - Contact');
   }
   imageStyle = { width: '32px', height: '32px' };
-  emailAddress: string = 'mahafathyh@gmail.com';
+  emailAddress: string = 'mailto:mahafathyh@gmail.com';
 
 }
